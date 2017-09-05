11ad6f00152a1d0bd304ca99be27921845a06544
RicardoVelazco <31299653+RicardoVelazco@users.noreply.github.com>
2017-09-05 13:14:52 -0500

    Add files via upload

---
+++ Archivo1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pylab as pl
+x = [5000,6000,7000,8000,9000]
+y = [65,78,88,89,93]
+pl.plot(x,y,'r')
+x1 = [7000,8000,9000,10000,11000]
+y1 = [65,75,85,86,90]
+pl.plot(x1,y1)
+pl.xlabel('volt')
+pl.ylabel('Eficiencia')
+pl.savefig('Archivo1.png')
